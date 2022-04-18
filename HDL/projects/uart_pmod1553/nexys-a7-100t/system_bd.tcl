1f3a2483efc39743db596467c2a5ea53c2b14e03
Jay Convertino <johnathan.convertino.1@us.af.mil>
2022-04-18 17:34:29 -0400

    changed uart to loop cts/rts (does nothing in the core... yet). Added nexys uart project. Build tested, not hardware approved

---
+++ HDL/projects/uart_pmod1553/nexys-a7-100t/system_bd.tcl
@@ -0,0 +1,3 @@
+
+source $ad_hdl_dir/projects/common/nexys-a7-100t/nexys-a7-100t_system_bd.tcl
+source ../common/uart_pmod1553_bd.tcl
--- HDL/projects/uart_pmod1553/nexys-a7-100t/system_top.v
+++ HDL/projects/uart_pmod1553/nexys-a7-100t/system_top.v
@@ -8,33 +8,49 @@
 
 module system_top (
   // clock and reset
-  input           clk_12mhz,
+  input           clk_100mhz,
+  input           resetn,
+  
   // general io
-  output  [3:0]   four_leds,
-  output  [2:0]   rgb_led,
-  input   [1:0]   push_buttons,
-  // 1553
+  output  [15:0]   leds,
+  
+  output  [2:0]   rgb_led0,
+  output  [2:0]   rgb_led1,
+  
+  input   [4:0]   push_buttons,
+  
+  input   [15:0]   slide_switches,
+  
+  // pmod
   inout   [7:0]   pmod_ja,
+  inout   [7:0]   pmod_jb,
+  inout   [7:0]   pmod_jc,
+  inout   [7:0]   pmod_jd,
+  
   // uart
   output           uart_tx,
-  input            uart_rx
+  input            uart_rx,
+  output           uart_rts,
+  input            uart_cts
   );
   
   system_wrapper i_system_wrapper (
-    .clk_12mhz(clk_12mhz),
-    .four_leds(four_leds),
-    .rgb_led(rgb_led),
+    .clk_100mhz(clk_100mhz),
+    .leds(leds),
+    .rgb_led0(rgb_led0),
+    .rgb_led1(rgb_led1),
     .push_buttons(push_buttons),
+    .slide_switches(slide_switches),
     .pmod_ja_pin1_i(pmod_ja[0]),
     .pmod_ja_pin2_i(pmod_ja[1]),
     .pmod_ja_pin3_o(pmod_ja[2]),
     .pmod_ja_pin4_o(pmod_ja[3]),
     .pmod_ja_pin5_o(pmod_ja[4]),
-    .reset(push_buttons[0]),
+    .resetn(resetn),
     .uart_rxd(uart_rx),
     .uart_txd(uart_tx),
-    .uart_rtsn(),
-    .uart_ctsn(1'b1)
+    .uart_rtsn(uart_rts),
+    .uart_ctsn(uart_cts)
   );
 
 endmodule
